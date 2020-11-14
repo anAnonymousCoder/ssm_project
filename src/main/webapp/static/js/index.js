2e52c1387b7cb19e256cc1f615b5b7ec13994538
CodePeasantry <528945404@qq.com>
2020-11-14 11:35:52 +0800

    update login page

---
+++ src/main/webapp/static/js/index.js
@@ -0,0 +1,43 @@
+layui.use(['form'], function () {
+    let form = layui.form
+        , layer = layui.layer;
+
+    //提交表单
+    form.on('submit(submitBtn)', function (data) {
+        let field = data.field;
+        let name = field.name, password = field.password;
+        let ajaxTimeOut = $.ajax({
+            url: '/ssm_project/rest/user/login',
+            type: 'get',
+            data: {
+                name: name,
+                password: password
+            },
+            success: function (res) {
+                console.log(res);
+                if (res.code === 2) {
+                    layer.msg("该用户不存在！");
+                    $("#name").val('');
+                } else {
+                    if (res.code === 0) {
+                        layer.msg("登录成功！");
+                        window.location.href = "pages/login-success.html" + encodeURI("?name=" + name);
+                    } else {
+                        layer.msg("密码错误！");
+                        $("#password").val('');
+                    }
+                }
+            },
+            error: function () {
+                layer.msg("请求发送错误");
+            },
+            complete: function (XMLHttpRequest, status) {
+                if (status === 'timeout') {//超时,status还有success,error等值的情况
+                    ajaxTimeOut.abort(); //取消请求
+                    layer.msg('请求超时，请重试', {icon: 2});
+                }
+            }
+        });
+        return false;
+    });
+});
